7cb75a6874e560eaf730f662e6ffbffc113d3fde
ABR <rajeshregar482@gmail.com>
2024-05-06 10:58:56 +0530

    Tic toc Toe Complete submission

--- lib/utils/audio_res.dart
+++ lib/utils/audio_res.dart
@@ -6,5 +6,6 @@ class AudioRes {
   static String get buzzer => "$audio/buzzer.mp3";
   static String get beep => "$audio/beep.mp3";
   static String get playfulCasino2 => "$audio/playful_casino_2.mp3";
+  static String get bitVideoGameLoseSound2 => "$audio/8_bit_video_game_lose_sound_2.mp3";
 
 }
